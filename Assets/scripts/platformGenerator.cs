f98cc5856f021de0653787665ace7859dc78cd28
Luis Pozo <lpozogilo@gmail.com>
2019-03-28 20:10:39 +0100

    Adding a Spawn platform

--- Assets/scripts/platformGenerator.cs
+++ Assets/scripts/platformGenerator.cs
@@ -6,6 +6,7 @@ public class platformGenerator : MonoBehaviour
 {
 
     public GameObject platform;
+    public GameObject spawnPlatform;
     //spawntime of platform
     public float spawnTime;
     //yUp max y on y axe for random spawn, yDown for min on y axe (maybe 3, 0 for test).
@@ -15,11 +16,13 @@ public class platformGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //call funktion that spawns your platform every spawnTime;
-        InvokeRepeating("platformSpawn", 0, spawnTime);
         yMin = -4.5f;
         yMax = 4.5f;
-        actualPosition = Random.Range(yMin, yMax);
+        Vector3 spawnPos = new Vector3(-2.5f, 0, 0);
+        Instantiate(spawnPlatform, spawnPos, transform.rotation);
+        actualPosition = -3;
+        //call funktion that spawns your platform every spawnTime;
+        InvokeRepeating("platformSpawn", 0, spawnTime);
     }
 
     // Update is called once per frame
