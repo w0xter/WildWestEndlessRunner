1362a8ba4acf2ec454edc239b2a910f214b6e255
Mathias Alvez <Mathias@MacBook-Pro.local>
2019-03-28 12:41:04 +0100

    Add Score Update Modified

---
+++ Assets/scripts/actualizarPuntuacion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class actualizarPuntuacion : MonoBehaviour {
+
+    public int score;
+    public Text scoreDisplay;
+
+    private void Update()
+    {
+        score += (int) Time.deltaTime;
+        scoreDisplay.text = score.ToString("Score: " + score);
+    }
+}
