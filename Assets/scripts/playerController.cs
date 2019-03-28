c759b40f6153e854020d9baf811d9d991c8a252a
Marsb10 <sebastian.mariano.ulloa@alumnos.upm.es>
2019-03-28 13:34:29 +0100

    Movimiento del jugador

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D playerRB;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        extraJumps = extraJumpsValue;
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxis("Horizontal");
+        playerRB.velocity = new Vector2(moveInput * speed, playerRB.velocity.y);
+    }
+
     void Update()
     {
-        
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        {
+            playerRB.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true)
+        {
+            playerRB.velocity = Vector2.up * jumpForce;
+        }
     }
 }
