17a6cfa9f562ad2a86d137e165e18990a9773a86
Marsb10 <sebastian.mariano.ulloa@alumnos.upm.es>
2019-03-29 15:31:42 +0100

    StartMenu PauseMenu y Gameover

---
+++ Assets/scripts/startMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startMenu : MonoBehaviour
+{
+    public string startGame;
+
+    public GameObject startMenuCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("METEDELJUEGOAQUI");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
