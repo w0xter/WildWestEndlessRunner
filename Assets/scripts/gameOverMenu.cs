03cd97b81900fea46e332bba48e4f14d8d21568b
Marsb10 <sebastian.mariano.ulloa@alumnos.upm.es>
2019-03-30 15:42:14 +0100

    menus a master

---
+++ Assets/scripts/gameOverMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameOverMenu : MonoBehaviour
+{
+    public string mainMenu;
+
+    public GameObject gameOverMenuCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("METEDELJUEGOAQUI");
+    }
+
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+
+}
