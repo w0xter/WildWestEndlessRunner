5847acfedd0bbba8d3f70db9e986ea95c1c0315d
Luis Pozo <lpozogilo@gmail.com>
2019-03-30 16:36:11 +0100

    adding platformGenerator again

--- Assets/scripts/platformGenerator.cs
+++ Assets/scripts/platformGenerator.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class platformGenerator : MonoBehaviour
 {
+	private float maxY;
+	private float minY;
+	public GameObject platform;
+	private float actualPosition;
+	public float spwanTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxY = 4.5f;
+        minY = -4.5f;
+        actualPosition = Random.Range(minY, maxY);
+        InvokeRepeating("spawnPlatform", 0, spwanTime);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void spawnPlatform(){
+    	float y  = generarSiguientePlataforma();
+    	Vector3 xyzPosition = new Vector3(transform.position.x, y, 0);
+    	Instantiate(platform, xyzPosition, transform.rotation);
+    }
+    private	float generarSiguientePlataforma(){
+    	float maxRango = actualPosition + 2;
+    	if(maxRango > maxY)
+    		maxRango = actualPosition;
+    	float y  = Random.Range(minY, maxRango);
+    	return y;
     }
 }
