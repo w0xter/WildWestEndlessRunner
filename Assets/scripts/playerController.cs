f121073a1285f65e94c0043955f8ae36a3050089
Mathias Alvez <Mathias@MacBook-Pro.local>
2019-04-01 17:20:10 +0200

    Add Audio Manager

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
-{
+public class playerController : MonoBehaviour {
     public float speed;
     public float jumpForce;
     private float moveInput;
 
     private Rigidbody2D playerRB;
-    public  GameObject player;
+    public GameObject player;
     public static float altura;
     public Animator anim;
     public static bool isGrounded;
@@ -19,53 +18,59 @@ public class playerController : MonoBehaviour
     public bool alive;
     private int extraJumps;
     public int extraJumpsValue;
-  //  private bool isAlive;
+    //  private bool isAlive;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start () {
 
         isAlive = player.transform.position.y > -4.5f;
         extraJumps = extraJumpsValue;
         //anim = GetComponent<Animator>();
-        playerRB = GetComponent<Rigidbody2D>();
-        groundCheck = GetComponent<Collider2D>();
+        playerRB = GetComponent<Rigidbody2D> ();
+        groundCheck = GetComponent<Collider2D> ();
+
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
+    void FixedUpdate () {
         isAlive = player.transform.position.y > -4.5f;
-        if(isAlive){
-            isGrounded = Physics2D.IsTouchingLayers(groundCheck, whatIsGround);
-            moveInput = Input.GetAxis("Horizontal");
-            playerRB.velocity = new Vector2(moveInput * speed, playerRB.velocity.y);
+        if (isAlive) {
+            isGrounded = Physics2D.IsTouchingLayers (groundCheck, whatIsGround);
+            moveInput = Input.GetAxis ("Horizontal");
+            playerRB.velocity = new Vector2 (moveInput * speed, playerRB.velocity.y);
         }
     }
 
-    void Update()
-    {
-         isAlive = player.transform.position.y > -4.5f;
-         alive = isAlive;
-         if(isAlive){
-         altura = player.transform.position.y;
-        if (isGrounded == true)
-        {
-            extraJumps = extraJumpsValue;
-            anim.SetBool("grounded", true);
-        }else{
-            anim.SetBool("grounded",false);
-        }
-        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && extraJumps > 0)
-        {
-            playerRB.velocity = Vector2.up * jumpForce;
-            extraJumps--;
-        }
-        else if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && extraJumps == 0 && isGrounded == true)
-        {
-            playerRB.velocity = Vector2.up * jumpForce;
+    void Update () {
+        isAlive = player.transform.position.y > -4.5f;
+        alive = isAlive;
+        if (isAlive) {
+            altura = player.transform.position.y;
+            if (isGrounded == true) {
+                extraJumps = extraJumpsValue;
+                StepAudio ();
+                anim.SetBool ("grounded", true);
+            } else {
+                anim.SetBool ("grounded", false);
+            }
+            if ((Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.UpArrow)) && extraJumps > 0) {
+                playerRB.velocity = Vector2.up * jumpForce;
+                JumpAudio ();
+                extraJumps--;
+            } else if ((Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.UpArrow)) && extraJumps == 0 && isGrounded == true) {
+                playerRB.velocity = Vector2.up * jumpForce;
+            }
         }
     }
-       
+
+    //This method is called from events in the animation itself. This keeps the footstep
+    //sounds in sync with the visuals
+    public void StepAudio () {
+        //Tell the Audio Manager to play a footstep sound
+        AudioManager.PlayFootstepAudio ();
+    }
+
+    public void JumpAudio () {
+        AudioManager.PlayJumpAudio ();
     }
 }
